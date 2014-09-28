fd287a2739614fc68ac0350cc1b53f74a69b0627
Mohammed Sameer <msameer@foolab.org>
2014-09-29 02:24:50 +0300

    BaseMapScreen: Cleaned up and left the minimal functionality needed for now

--- src/com/omareitti/BaseMapScreen.java
+++ src/com/omareitti/BaseMapScreen.java
@@ -1,86 +1,27 @@
 package com.omareitti;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import java.util.prefs.Preferences;
-
-import com.omareitti.IBackgroundServiceAPI;
-import com.omareitti.IBackgroundServiceListener;
-import com.omareitti.R;
-
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
-import org.osmdroid.views.overlay.Overlay;
-
-/*
-import com.google.android.maps.MyLocationOverlay;
-import com.google.android.maps.Projection;
-*/
-
-import com.omareitti.Route.PathSegment;
-import com.omareitti.Route.RouteStep;
-import com.omareitti.datatypes.GeoRec;
-
 import android.app.Activity;
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.Paint;
-import android.graphics.Path;
-import android.graphics.Point;
-import android.graphics.drawable.Drawable;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.os.RemoteException;
 import android.preference.PreferenceManager;
-import android.util.AttributeSet;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-import android.view.MenuInflater;
-import android.os.AsyncTask;
-import android.app.ProgressDialog;
-import com.omareitti.datatypes.Coords;
+import android.view.MenuItem;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
 
 public class BaseMapScreen extends Activity {
     private static final String TAG = BaseMapScreen.class.getSimpleName();
 
-    LinearLayout linearLayout;
-    MapView mapView;
-
-    private Paint mPaint;
-    public Route route = null;
-    private String routeString;
-    private int currentStep = 0;
-    private int zoomLevel;
-    private String pickPoint;
-    Bitmap markerBmp;
-    //private MyLocationOverlay myLocOverlay;
-    private Boolean isJustLooking = false;
-    SharedPreferences prefs;
-    private MapController myMapController;
-    private GeoPoint currentPoint = null;
-    private float locationAngle = 0f;
-    private Boolean takeLocation = false;
+    MapView mView;
+    private MapController mController;
+    private MyLocationNewOverlay mLocation;
+    private CompassOverlay mCompass;
 
     @Override
     protected void onCreate(Bundle icicle) {
@@ -90,115 +31,32 @@ public class BaseMapScreen extends Activity {
 
 	setContentView(R.layout.mapscreen);
 
-	if (getIntent().getExtras() == null) {
-            startActivity(new Intent(BaseMapScreen.this, MainApp.class));
-            Log.i(TAG, "intent extras is null, switching to main activity ");
-            finish();
-            return;
-	}
+	mView = (MapView) findViewById(R.id.mapview);
+	mView.setBuiltInZoomControls(true);
 
-	routeString = getIntent().getExtras().getString("route");
+	// TODO: a menu with available tile sources
+	////	mView.setSatellite(false);
+	////	mView.setStreetView(false);
 
-	mSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-        mSensorManager.registerListener(mListener, mSensor,
-					SensorManager.SENSOR_DELAY_GAME);
+	SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+	int zoomLevel = Integer.parseInt(prefs.getString("prefMapZoomLevel", "15"));
 
-        makeArrow();
-	isJustLooking = getIntent().getExtras().getBoolean("isJustLooking", false);
-	pickPoint = getIntent().getExtras().getString("pickPoint");
+	mController = (MapController)mView.getController();
+	mController.setZoom(zoomLevel);
 
-	Log.i(TAG, "pickPoint: "+pickPoint);
+	mController.setCenter(new GeoPoint(60.170833, 24.9375));
 
-	mapView = (MapView) findViewById(R.id.mapview);
-	mapView.setBuiltInZoomControls(true);
-	////	mapView.setSatellite(false);
-	////	mapView.setStreetView(false);
+	mLocation = new MyLocationNewOverlay(BaseMapScreen.this, mView);
+	mLocation.setDrawAccuracyEnabled(true);
+	mView.getOverlays().add(mLocation);
 
-	prefs = PreferenceManager.getDefaultSharedPreferences(this);
-	zoomLevel = Integer.parseInt(prefs.getString("prefMapZoomLevel", "15"));
-
-	myMapController = (MapController)mapView.getController();
-	myMapController.setZoom(zoomLevel);
-
-	myMapController.setCenter(new GeoPoint(6016265,24915534));
-
-        mPaint = new Paint();
-        mPaint.setDither(true);
-        mPaint.setColor(Color.BLUE);
-        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
-        mPaint.setStrokeJoin(Paint.Join.ROUND);
-        mPaint.setStrokeCap(Paint.Cap.ROUND);
-        mPaint.setStrokeWidth(5);
-
-	Log.i(TAG, "trying to bind service "+BackgroundService.class.getName());
-	Intent servIntent = new Intent(BackgroundService.class.getName());//this, BackgroundService.class);
-        startService(servIntent);
-        Log.i(TAG, "starting service "+servIntent.toString());
-        bindService(servIntent, servceConection, 0);
-
-        //myLocOverlay = new MyLocationOverlay(this, mapView);
-	//myLocOverlay.enableMyLocation();
-	//mapView.getOverlays().add(myLocOverlay);
-
-	if (pickPoint != null) {
-
-	    MapOverlay mapOverlay = new MapOverlay(BaseMapScreen.this);
-	    mapView.getOverlays().add(mapOverlay);
-
-	    mapView.invalidate();
-	    Toast.makeText(this, getString(R.string.msToastAddressSelect), Toast.LENGTH_SHORT).show();
-	    takeLocation = true;
-	    return;
-	}
-
-    	try {
-
-	    if (!routeString.equals("")) route = new Route(routeString);
-	    else {
-		Log.i(TAG, "Couldn't get the route from JSONobj "+routeString);
-		Toast.makeText(this, "Can't get current route", Toast.LENGTH_LONG);
-		startActivity(new Intent(BaseMapScreen.this, MainApp.class));
-		finish();
-                return;
-	    }
-    	} catch ( Exception e ) {
-	    Log.e(TAG, "Couldn't get the route from JSONobj "+routeString, e);
-    	}
-
-        currentStep = getIntent().getExtras().getInt("currentStep");
-
-	mapView.getOverlays().add(new RouteOverlay(BaseMapScreen.this));
-
-	if (route != null && route.steps.size() > currentStep) {
-	    RouteStep r = route.steps.get(currentStep);
-	    if (r.path.size() > 0) {
-		PathSegment p = r.path.get(0);
-		myMapController.setCenter(new GeoPoint((int) (p.coords.x * 1E6), (int) (p.coords.y * 1E6)));
-	    }
-	}
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-	MenuInflater inflater = getMenuInflater();
-	inflater.inflate(R.menu.mapscreen_actions, menu);
-
-    	return super.onCreateOptionsMenu(menu);
+	mCompass = new CompassOverlay(BaseMapScreen.this, mView);
+	mView.getOverlays().add(mCompass);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 	switch (item.getItemId()) {
-	case R.id.action_show_mylocation:
-	    if (currentPoint == null) {
-		takeLocation = true;
-		return true;
-	    }
-
-	    myMapController.animateTo(currentPoint);
-	    break;
-
 	case android.R.id.home:
             onBackPressed();
 	    return true;
@@ -213,297 +71,33 @@ public class BaseMapScreen extends Activity {
     @Override
     protected void onDestroy() {
 	super.onDestroy();
-	mSensorManager.unregisterListener(mListener);
-	try {
-	    if (api != null) api.removeListener(serviceListener);
-	    unbindService(servceConection);
-	    Log.i(TAG, "unbind ");
-	} catch(Exception e) {
-	    Log.e(TAG, "ERROR!!", e);
-	}
     }
 
-    class MapOverlay extends Overlay {
-	MapOverlay(Context context) {
-	    super(context);
-	}
+    @Override
+    protected void onPause() {
+	super.onPause();
 
-	@Override
-	    public void draw(Canvas canvas, MapView mapView, boolean shadow) {
-
-	    if (currentPoint == null) {
-		return;
-	    }
-
-	    Point screenPts = new Point();
-	    mapView.getProjection().toPixels(currentPoint, screenPts);
-
-	    drawArrow(canvas, screenPts.x, screenPts.y);
-	}
-
-	private long lastTouchTime;
-	@Override
-	    public boolean onTouchEvent(MotionEvent event, MapView mapView) {
-	    if (pickPoint == null) {
-		return super.onTouchEvent(event, mapView);
-	    }
-
-	    if (event.getAction() == MotionEvent.ACTION_DOWN) {
-		long thisTime = System.currentTimeMillis();
-		if (thisTime - lastTouchTime > 250) {
-		    // Too slow :)
-		    lastTouchTime = thisTime;
-		    return false;
-     		} else {
-     		    // Double tap
-     		    lastTouchTime = -1;
-     		}
-
-		GeoPoint p =
-		    (GeoPoint)mapView.getProjection().fromPixels((int) event.getX(),
-								 (int) event.getY());
-
-		if (p == null)
-		    return false;
-
-		ProgressDialog dlg =
-		    ProgressDialog.show(BaseMapScreen.this, "",
-					getString(R.string.maDlgSearch), true);
-		dlg.show();
-
-		new Geocode(dlg, p).execute(p);
-		return true;
-     	    } else
-     		return false;
-    	}
+	mCompass.disableCompass();
+	mLocation.disableMyLocation();
     }
 
-    class RouteOverlay extends Overlay {
-	RouteOverlay(Context context) {
-	    super(context);
-	}
+    @Override
+    protected void onResume() {
+	super.onResume();
 
-	@Override
-	    public void draw(Canvas canvas, MapView mapv, boolean shadow){
-
-	    if (route == null) return;
-
-	    Point p1 = new Point();
-	    Point p2 = new Point();
-
-	    for (int i = 0; i < route.steps.size(); i++) {
-		Path path = new Path();
-
-		RouteStep r = route.steps.get(i);
-		mPaint.setColor(r.getColor());
-
-		for (int j = 0; j < r.path.size(); j++) {
-		    PathSegment p = r.path.get(j);
-
-		    int x = (int) (p.coords.x * 1E6);
-     		    int y = (int) (p.coords.y * 1E6);
-	        		
-     		    GeoPoint gP1 = new GeoPoint(x, y);
-     		    mapView.getProjection().toPixels(gP1, p1);
-	        		
-     		    if (j == 0)  {
-     			path.moveTo(p1.x, p1.y);
-     			//canvas.drawCircle(p1.x, p1.y, 4, mPaint);
-     			path.addCircle(p1.x, p1.y, 5, Path.Direction.CW);
-     		    }
-     		    else {
-     			path.moveTo(p2.x, p2.y);
-     			path.addCircle(p2.x, p2.y, 3, Path.Direction.CW);
-     			path.lineTo(p1.x, p1.y);
-     			//canvas.drawCircle(p1.x, p1.y, 3, mPaint);
-     		    }
-	        		
-     		    p2.x = p1.x; p2.y = p1.y; 
-     		    //Log.i(TAG, "path:"+j+" x:"+p1.x+" y:"+p1.y);
-     		}
-     		canvas.drawPath(path, mPaint);
-     	    }
-     	    if (currentPoint != null) {
-     		Point screenPts = new Point();
-     		mapView.getProjection().toPixels(currentPoint, screenPts);
-
-     		drawArrow(canvas, screenPts.x, screenPts.y);
-     	    }
-     	}
-		
-     	@Override
-     	    public boolean onTouchEvent(MotionEvent event, MapView mapView) {
-     	    return super.onTouchEvent(event, mapView);
-    	}
+	mCompass.enableCompass();
+	mLocation.enableMyLocation();
     }
 
-    /** 
-     * Service interaction stuff
-     */
-    private IBackgroundServiceListener serviceListener = new IBackgroundServiceListener.Stub() {
- 		
-	    public void locationDiscovered(double lat, double lon)
-		throws RemoteException {
-		//Log.i(TAG, "locationDiscovered: "+lat+" "+lon);
-		//if (pickPoint != null) {
-		int ilat = (int) (lat * 1E6);
-		int ilng = (int) (lon * 1E6);
-		currentPoint = new GeoPoint(ilat, ilng);
-		myMapController.animateTo(currentPoint); //setCenter	
-		locationAngle = 0f;
-		//}
-	    }
-		
-	    public void handleUpdate(String s) throws RemoteException {
-		//Log.i(TAG, "handleUpdate: "+s);
-	    }
-		
-	    public void handleGPSUpdate(double lat, double lon, float angle) throws RemoteException {
-		if (!takeLocation && pickPoint != null) return;
-			
-        	int ilat = (int) (lat * 1E6);
-        	int ilng = (int) (lon * 1E6);
-        	currentPoint = new GeoPoint(ilat, ilng);
-        	
-		if (!isJustLooking)
-		    myMapController.animateTo(currentPoint); //setCenter
-        	
-        	if (takeLocation) takeLocation = false;
-
-        	locationAngle = angle;
-        	
-		//setContentView(mapView);
-	    }
-		
-	    public void addressDiscovered(String address) throws RemoteException {
-		
-	    }
-	};
-
-	
-    private IBackgroundServiceAPI api = null;
-	
-    private ServiceConnection servceConection = new ServiceConnection() {
-		
-	    public void onServiceDisconnected(ComponentName name) {
-		Log.i(TAG, "Service disconnected!");
-		api = null;
-	    }
-		
-	    public void onServiceConnected(ComponentName name, IBinder service) {
-		api = IBackgroundServiceAPI.Stub.asInterface(service);
-
-		locationAngle = 0f;
-		Log.i(TAG, "Service connected! "+api.toString());
-		try {
-		    api.addListener(serviceListener);
-		    //if (pickPoint != null) {
-		    api.requestLastKnownAddress(0);
-		    //}
-		} catch(Exception e) {
-		    Log.e(TAG, "ERROR!!", e);
-		}
-	    }
-	};
-	
-    private Paint   arrowPaint = new Paint();
-    private Path    arrowPath = new Path();
-    private void makeArrow() {
-	arrowPath.moveTo(0, -30);
-	arrowPath.lineTo(-20, 30);
-	arrowPath.lineTo(0, 20);
-	arrowPath.lineTo(20, 30);
-	arrowPath.close();	
-    }
-    private void drawArrow(Canvas canvas, int cx, int cy) {
-	/*int w = canvas.getWidth();
-	  int h = canvas.getHeight();
-	  int cx = w / 2;
-	  int cy = h / 2;*/
-	Paint paint = arrowPaint;
-
-        paint.setAntiAlias(true);
-        //paint.setColor(Color.BLUE);
-        paint.setStyle(Paint.Style.FILL);		
-
-        canvas.save();
-        canvas.translate(cx, cy);
-        //Log.i(TAG, "ANGLE: "+locationAngle+" "+mValues);
-        if (locationAngle == 0 && mValues != null) {
-	    paint.setColor(Color.BLUE);
-	    canvas.rotate(mValues[0]);
-        } else {
-	    paint.setColor(Color.GREEN);
-	    canvas.rotate(locationAngle);
-        }
-        
-        canvas.drawPath(arrowPath, arrowPaint);
-        /*if (mValues != null) {
-	  canvas.rotate(mValues[0]);
-	  } 
-	  canvas.translate(-cx, -cy);*/
-        canvas.restore();
-    }
-	
-    private SensorManager mSensorManager;
-    private Sensor mSensor;
-
-    private float[] mValues;
-
-    private final SensorEventListener mListener = new SensorEventListener() {
-	    public void onSensorChanged(SensorEvent event) {
-		/*Log.d(TAG,
-		  "sensorChanged (" + event.values[0] + ", " + event.values[1] + ", " + event.values[2] + ")");*/
-		mValues = event.values;
-	    }
-
-	    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-	    }
-	};
-
-    private class Geocode extends AsyncTask<GeoPoint, Integer, String> {
-	public Geocode(ProgressDialog dlg, GeoPoint p) {
-	    super();
-
-	    mDlg = dlg;
-	    mPoint = p;
-	}
-
-	@Override
-	protected String doInBackground(GeoPoint... points) {
-	    GeoPoint p = points[0];
-
-	    ArrayList<GeoRec> recs =
-		ReittiopasAPI.getReverseGeocode(new Coords(p.getLongitudeE6() / 1E6,
-							   p.getLatitudeE6() / 1E6).toString());
-	    if (recs == null || recs.size() == 0)
-		return null;
-
-	    return recs.get(0).name;
-	}
-
-	@Override
-	protected void onPostExecute(String result) {
-	    mDlg.dismiss();
-
-	    if (result == null || result.length() == 0) {
-		MainApp.showErrorDialog(BaseMapScreen.this,
-					getString(R.string.error),
-					getString(R.string.msFailedAddress));
-	    } else {
-		Toast.makeText(getBaseContext(), result, Toast.LENGTH_SHORT).show();
-		Intent intent = new Intent();
-		intent.putExtra("mapAddress", result);
-		intent.putExtra("mapCoords",
-				new Coords(mPoint.getLongitudeE6() / 1E6,
-					   mPoint.getLatitudeE6() / 1E6).toString());
-		setResult(RESULT_OK, intent);
-		finish();
-	    }
-	}
-
-	private ProgressDialog mDlg;
-	private GeoPoint mPoint;
+    public MapView getMapView() {
+	return mView;
     }
 
+    public void goToCurrentLocation() {
+	// TODO: feedback if p is null
+	GeoPoint p = mLocation.getMyLocation();
+
+	if (p != null)
+	    mController.animateTo(p);
+    }
 }
