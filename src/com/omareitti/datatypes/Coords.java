d2a01ce4d12ae865df860031d0ccf0fc0843c5a7
Mohammed Sameer <msameer@foolab.org>
2014-09-29 23:47:25 +0300

    Coords: reworked parsing coordinates from strings and add logging

--- src/com/omareitti/datatypes/Coords.java
+++ src/com/omareitti/datatypes/Coords.java
@@ -1,28 +1,38 @@
 package com.omareitti.datatypes;
 
+import android.util.Log;
+
+// x is longitude. y is latitude
+
 public class Coords {
+    private static final String TAG = Coords.class.getSimpleName();
+
     public double x;
     public double y;
 
     public Coords(String coord) {
-	int pos = coord.indexOf(",");
-	this.y = Double.parseDouble(coord.substring(pos+1, coord.length()));
-	this.x = Double.parseDouble(coord.substring(0, pos));
+	String p[] = coord.split(",");
+	x = Double.parseDouble(p[0]);
+	y = Double.parseDouble(p[1]);
+
+	Log.i(TAG, "Created new with longitude = " + x + " and latitude = " + y);
     }
-	
+
     public Coords(Double x, Double y) {
 	this.y = y;
 	this.x = x;
+
+	Log.i(TAG, "Created new with longitude = " + x + " and latitude = " + y);
     }
-	
+
     public String toString() {
 	return ""+x+","+y;
     }
-	
+
     public int xToInt() {
 	return (int) (x*1E6);
     }
-	
+
     public int yToInt() {
 	return (int) (y*1E6);
     }
