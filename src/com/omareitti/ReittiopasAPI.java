c733ad1bfee2e812230e702516dbd0d056c85327
Mohammed Sameer <msameer@foolab.org>
2014-09-30 21:49:19 +0300

    indentation

--- src/com/omareitti/ReittiopasAPI.java
+++ src/com/omareitti/ReittiopasAPI.java
@@ -29,20 +29,23 @@ import android.util.Log;
 
 public class ReittiopasAPI {
     private static final String TAG = "ReittiopasAPI";
-	
-    private static final String main_url = "http://api.reittiopas.fi/hsl/1_1_2/?request="; //"http://api.reittiopas.fi/hsl/prod/?request=";
-    private static final String auth = "&user=difogic&pass=routeapi$44&epsg_out=4326&epsg_in=4326&show=5"; 
+
+    private static final String main_url =
+	"http://api.reittiopas.fi/hsl/1_1_2/?request=";
+
+    private static final String auth =
+	"&user=difogic&pass=routeapi$44&epsg_out=4326&epsg_in=4326&show=5"; 
 
     static DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
     static DateFormat timeFormat = new SimpleDateFormat("HHmm");
-	
-    public static int walkingSpeed = 70; // Walking speed. Optional, default 70 m/min, range 1-500.
-		
-	
+
+    // Walking speed. Optional, default 70 m/min, range 1-500.
+    public static int walkingSpeed = 70;
+
     public interface RequestReceiver {
 	public void handleRequest(String content);
     }
-	
+
     public static String formatDate(Date dt) {
 	return dateFormat.format(dt);
     }
@@ -53,28 +56,40 @@ public class ReittiopasAPI {
 
     public static ArrayList<GeoRec> getGeocode(String search) {
 	String url = main_url+"geocode"+auth+"&key="+search.replace(" ", "+").toLowerCase();
-	//Log.i(TAG, "URL: "+url);
 	return Route.getGeocodeCoords(queryUrl(url));
     }
 
     public static ArrayList<GeoRec> getReverseGeocode(String data) {
 	String url = main_url+"reverse_geocode"+auth+"&coordinate="+data.replace(" ", "+");
-	//Log.i(TAG, "URL: "+url);
 	return Route.getGeocodeCoords(queryUrl(url));
-    }	
-	
-    public ArrayList<Route> getRoute(String from, String to, String date, String time, String optimize, String timetype, String transport_types) {
-	if (from == null || to == null) return null;
+    }
+
+    public ArrayList<Route> getRoute(String from, String to, String date,
+				     String time, String optimize,
+				     String timetype, String transport_types) {
+
+	if (from == null || to == null)
+	    return null;
 
         Date dt = new Date();
-		
-	if (date.equals("")) date = dateFormat.format(dt);
-	if (time.equals("")) time = timeFormat.format(dt);
-	if (optimize.equals("")) optimize = "default";
-	if (timetype.equals("")) timetype = "departure"; // "arrival" or "departure"
-	if (transport_types.equals("")) transport_types = "all";
+
+	if (date.equals(""))
+	    date = dateFormat.format(dt);
+
+	if (time.equals(""))
+	    time = timeFormat.format(dt);
+
+	if (optimize.equals(""))
+	    optimize = "default";
+
+	if (timetype.equals(""))
+	    timetype = "departure"; // "arrival" or "departure"
+
+	if (transport_types.equals(""))
+	    transport_types = "all";
+
 	transport_types = transport_types.replace("|", "%7C");
-		
+
 	String url = main_url+"route"+auth+"&from="+from.toLowerCase()
 	    +"&to="+to.toLowerCase()
 	    +"&date="+date
@@ -84,23 +99,14 @@ public class ReittiopasAPI {
 	    +"&transport_types="+transport_types
 	    +"&walk_speed="+walkingSpeed
 	    +"&show=5";
-		
-	//Log.i(TAG, "URL: "+url);
-		
+
 	String content = queryUrl(url);
 	ArrayList<Route> routes = Route.parseRoute(content);
 	return routes;
-    }		
-	
-    public void test() {  
-	//getGeocode("Matinraitti 5");
-	//getReverseGeocode("24.7361910358,60.1606744939");
-	getRoute("24.7361910358,60.1606744939", "24.9305257301,60.1681327014", "", "", "", "", "");
     }
-	
+
     public void get(final String url, final RequestReceiver rr) {
 	new Thread(new Runnable() {
-			
 		public void run() {
 		    String res = queryUrl(url);
 		    rr.handleRequest(res);
